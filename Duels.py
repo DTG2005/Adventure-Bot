3e2805c62328d1eff6582889ada9f39ae5d7ba19
DaAccursed05 <divyamthegreat5@gmail.com>
2021-03-19 16:23:19 +0530

    Some minor indentation changes.

--- Duels.py
+++ Duels.py
@@ -22,29 +22,29 @@ class Duels(commands.Cog):
                 await ctx.send("You can't go into a second duel with an ongoing duel.")
             elif user.id == ctx.author.id:
                 await ctx.send("You killed yourself in duel. Wait...you can't, duel somebody else.")
-
-        if user.bot:
-            await ctx.send("You can't duel bots, they can kill you with their botrickery!!")
-        else:
-
-            await ctx.send(f"{user.name}, you have been challenged by {ctx.author.mention} for a duel. Do you accept? Type 'Y' to Accept and 'N' to reject.")
-
-            def check(m):
-                return m.author.id == user.id and m.channel.id == ctx.channel.id and m.content in ['Y', 'y', 'n', 'N']
-
-            try:
-                response = await self.bot.wait_for('message', check=check, timeout= 30.0)
-            except asyncio.TimeoutError:
-                await ctx.send("Timeout exceeded, duel cannot be started.")
-                return
-
             else:
-                if response.content in ['y', 'Y']:
-                    self.bot.games[ctx.message.channel.id] = [ctx.author.id, user.id]
-                    await ctx.send("Duel has started. May the best warrior win!")
-
+                if user.bot:
+                    await ctx.send("You can't duel bots, they can kill you with their botrickery!!")
                 else:
-                    await ctx.send("Request denied! Duel cancelled.")
+
+                    await ctx.send(f"{user.name}, you have been challenged by {ctx.author.mention} for a duel. Do you accept? Type 'Y' to Accept and 'N' to reject.")
+
+                    def check(m):
+                        return m.author.id == user.id and m.channel.id == ctx.channel.id and m.content in ['Y', 'y', 'n', 'N']
+
+                    try:
+                        response = await self.bot.wait_for('message', check=check, timeout= 30.0)
+                    except asyncio.TimeoutError:
+                        await ctx.send("Timeout exceeded, duel cannot be started.")
+                        return
+
+                    else:
+                        if response.content in ['y', 'Y']:
+                            self.bot.games[ctx.message.channel.id] = [ctx.author.id, user.id]
+                            await ctx.send("Duel has started. May the best warrior win!")
+
+                        else:
+                            await ctx.send("Request denied! Duel cancelled.")
 
 def setup(bot):
     bot.add_cog(Duels(bot))
